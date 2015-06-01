62bb98bda78788af200bb50c3f3cec8606345ec3
carwestsam <carwestsam@gmail.com>
2015-06-01 17:08:35 +0800

    update at 20150601

---
+++ backend_bak.php
@@ -0,0 +1,52 @@
+<?php
+header( "Access-Control-Allow-Origin: *");
+
+
+/*
+ * Get Params
+ */
+
+$startTime = "";
+$endTime = "";
+
+
+$startTimeSQL = " ";
+$endTimeSQL = " ";
+
+$startTime = $_GET["startTime"];
+$endTime = $_GET["endTime"];
+
+
+if ( !empty($startTime)) {
+    $startTimeSQL = " pubdate > date (\"$startTime\") ";
+}
+if ( !empty($endTime)) {
+    $endTimeSQL = "pubdate < date (\"$endTime\") ";
+}
+
+/* End of file filename.php */
+$host="host=127.0.0.1" ;
+$port="port=5432" ;
+$dbname="dbname=news" ;
+$credentials="user=carwest password=123qwe!@#QWE" ;
+$db=pg_connect( "$host $port $dbname $credentials" );
+if (!$db){
+    echo "Error : unable to connect";
+}else { //echo "Opened database success";
+}
+
+$query='SELECT * from locations limit 100 where ' . $startTimeSQL . ' and ' . $endTimeSQL ;
+$result=pg_query($query) or die( 'Query failed:'.pg_last_error() );
+$ans_stack=array();
+echo "[";
+while ( $ro=pg_fetch_object($result) )
+{
+    //array_push( $ans_stack, $ro ); //echo $ro;
+    echo json_encode($ro);
+    echo ",";
+}
+echo "{}]";
+pg_free_result($result);
+pg_close( $db );
+?>
+
